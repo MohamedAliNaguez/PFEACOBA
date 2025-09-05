04e292e7573ad042bba60d7adea0f4726f96d550
unknown <mohamedali.naguez@esprit.tn>
2025-09-05 13:19:38 +0100

    fixing wdio

--- CMS_Android_E2E_tests/wdio.conf.js
+++ CMS_Android_E2E_tests/wdio.conf.js
@@ -1,61 +1,66 @@
 exports.config = {
-    runner: 'local',
+  runner: "local",
 
-    specs: [
-        './test/specs/**/*.js'
-    ],
+  specs: ["./test/specs/**/*.js"],
 
-    exclude: [],
+  exclude: [],
 
-    maxInstances: 1, // safer for Android emulators
+  maxInstances: 1, // safer for Android emulators
 
-    capabilities: [{
-        platformName: 'Android',
-        'appium:deviceName': 'emulator-5554',
-        'appium:automationName': 'UiAutomator2',
-        'appium:platformVersion': '15',
-        'appium:appPackage': 'com.acoba.cms.id.stg',
-        'appium:appActivity': 'com.acoba.cms.id.MainActivity',
-        'appium:noReset': true,
-        'appium:autoGrantPermissions': true
-    }],
-
-    logLevel: 'info',
-    bail: 0,
-    waitforTimeout: 10000,
-    connectionRetryTimeout: 120000,
-    connectionRetryCount: 3,
-
-    services: [
-        ['appium', {
-            command: 'C:\\Users\\Daly\\AppData\\Roaming\\npm\\appium.cmd',
-            args: {
-                relaxedSecurity: true,
-                logLevel: 'info',
-                port: 4723
-            }
-        }]
-    ],
-
-    framework: 'mocha',
-
-    reporters: [
-        'spec',
-        ['allure', {
-            outputDir: 'allure-results',
-            disableWebdriverStepsReporting: true,
-            disableWebdriverScreenshotsReporting: false
-        }]
-    ],
-
-    afterTest: function(test, context, { error }) {
-        if (error) {
-            browser.takeScreenshot();
-        }
+  capabilities: [
+    {
+      platformName: "Android",
+      "appium:deviceName": "emulator-5554",
+      "appium:automationName": "UiAutomator2",
+      "appium:platformVersion": "15",
+      "appium:appPackage": "com.acoba.cms.id.stg",
+      "appium:appActivity": "com.acoba.cms.id.MainActivity",
+      "appium:noReset": true,
+      "appium:autoGrantPermissions": true,
     },
+  ],
 
-    mochaOpts: {
-        ui: 'bdd',
-        timeout: 60000
-    },
-}
+  logLevel: "info",
+  bail: 0,
+  waitforTimeout: 10000,
+  connectionRetryTimeout: 120000,
+  connectionRetryCount: 3,
+
+  services: [
+    [
+      "appium",
+      {
+        args: {
+          relaxedSecurity: true,
+          logLevel: "info",
+          port: 4723,
+        },
+      },
+    ],
+  ],
+
+  framework: "mocha",
+
+  reporters: [
+    "spec",
+    [
+      "allure",
+      {
+        outputDir: "allure-results",
+        disableWebdriverStepsReporting: true,
+        disableWebdriverScreenshotsReporting: false,
+      },
+    ],
+  ],
+
+  afterTest: function (test, context, { error }) {
+    if (error) {
+      browser.takeScreenshot();
+    }
+  },
+
+  mochaOpts: {
+    ui: "bdd",
+    timeout: 60000,
+  },
+};
